601e6558f56fe37d16f1a73471a6b33b72666eab
Shotaro Maruyama <testSample@aaa.com>
2020-10-26 16:44:52 +0900

    first commit

---
+++ Assignment5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Assignment5
+//
+//  Created by 丸山翔太郎 on 2020/10/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
